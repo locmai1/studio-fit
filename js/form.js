f343eef271c87e39c10e04e54408e0fbe7639c80
Loc Mai <locmai0315@gmail.com>
2023-10-26 13:27:27 -0400

    move form validation to function

--- js/form.js
+++ js/form.js
@@ -1,31 +1,39 @@
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (e) {
-    const nameInput = document.getElementById("name");
-    const emailInput = document.getElementById("email");
-    const messageInput = document.getElementById("message");
-    const nameError = document.getElementById("name-error");
-    const emailError = document.getElementById("email-error");
-    const phoneError = document.getElementById("phone-error");
-    const messageError = document.getElementById("message-error");
+function validateForm() {
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
+  const nameError = document.getElementById("name-error");
+  const emailError = document.getElementById("email-error");
+  const phoneError = document.getElementById("phone-error");
+  const messageError = document.getElementById("message-error");
 
-    nameError.textContent = "";
-    emailError.textContent = "";
-    phoneError.textContent = "";
-    messageError.textContent = "";
+  nameError.textContent = "";
+  emailError.textContent = "";
+  phoneError.textContent = "";
+  messageError.textContent = "";
 
-    if (nameInput.value == "") {
-      nameError.textContent = "Name is required";
-      e.preventDefault();
-    }
+  var isNameError = false;
+  var isEmailError = false;
+  var isMessageError = false;
 
-    if (emailInput.value == "") {
-      emailError.textContent = "Email is required";
-      e.preventDefault();
-    }
+  if (nameInput.value == "") {
+    nameError.textContent = "Name is required";
+    isNameError = true;
+  }
 
-    if (messageInput.value == "") {
-      messageError.textContent = "Message is required";
-      e.preventDefault();
-    }
-  });
+  if (emailInput.value == "") {
+    emailError.textContent = "Email is required";
+    isEmailError = true;
+  }
+
+  if (messageInput.value == "") {
+    messageError.textContent = "Message is required";
+    isMessageError = true;
+  }
+
+  if (isNameError || isEmailError || isMessageError) {
+    return false;
+  }
+
+  return true;
+}
